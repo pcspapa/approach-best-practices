6c613242c421ad95f3b412917e46daa7f206f355
박찬석 <pcspapa@gmail.com>
2020-10-05 20:13:01 +0900

    first commit

---
+++ src/main/java/com/cspark/play/training/ApproachBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.cspark.play.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApproachBestPracticesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApproachBestPracticesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/cspark/play/training/ApproachBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cspark.play.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApproachBestPracticesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
